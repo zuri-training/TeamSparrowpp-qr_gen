600d4dc6f03d8a344d4ee7db18a2b3b929d006dc
Samuel Ibanga <ibangajnr27@gmail.com>
2022-12-09 06:05:50 +0100

    Created new django app for pages

---
+++ pages/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def homePage(request):
+    return render(request, "pages/landing_page.html")
+
+def dashboard(request):
+    return render(request, "pages/dashboard.html")
