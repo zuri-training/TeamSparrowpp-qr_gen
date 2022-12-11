be935036fe92f5fbcbe4d3dfbaee4d0984093a58
Samuel Ibanga <ibangajnr27@gmail.com>
2022-12-11 14:02:22 +0100

    Updates

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,18 +4,26 @@ from django.contrib.auth import get_user_model
 from .models import CustomUser
 
 class UserCreateForm(UserCreationForm):
-    # password1 = forms.CharField(
-    #     label=_("Password"),
-    #     strip=False,
-    #     widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
-    #     help_text=password_validation.password_validators_help_text_html(),
-    # )
-    # password2 = forms.CharField(
-    #     label=_("Password confirmation"),
-    #     widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
-    #     strip=False,
-    #     help_text=_("Enter the same password as before, for verification."),
-    # )
+    username = forms.CharField(
+        label=("Username"),
+        strip=False,
+        widget=forms.TextInput(attrs={"id": "username", "placeholder": "Username"}),
+    )
+    email = forms.CharField(
+        label=("Email address"),
+        strip=False,
+        widget=forms.EmailInput(attrs={'autocomplete': 'new-email', "id": "email", "placeholder": "sample@gmail.com"}),
+    )
+    password1 = forms.CharField(
+        label=("Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', "id": "password1", "placeholder": "Enter password here"}),
+    )
+    password2 = forms.CharField(
+        label= ("Password confirmation"),
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password', "id": "password2", "placeholder": "Enter password here"}),
+        strip=False,
+    )
     class Meta:
         model = get_user_model()
         fields = ['username', 'email', 'password1', 'password2']
