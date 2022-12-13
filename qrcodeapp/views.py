0439a4d6a6566543ae89153cdd7d2a3f831ef946
nadisan <nadiawellela@gmail.com>
2022-12-13 08:50:34 +0300

    qrcodeapp

---
+++ qrcodeapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.shortcuts import render
+
+import qrcode.image.svg
+from io import BytesIO
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+@login_required(login_url="login")
+def URL(request):
+    context = {}
+    if request.method == "POST":
+        factory = qrcode.image.svg.SvgImage
+        img = qrcode.make(request.POST.get("qr_text",""), image_factory=factory, box_size=20)
+        stream = BytesIO()
+        img.save(stream)
+        context["svg"] = stream.getvalue().decode()
+
+    return render(request, "URL.html", context=context)
+# Create your views here.
