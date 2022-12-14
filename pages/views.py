a964c277974b1ba536870163f4ea0a6d3a3c78db
Samuel Ibanga <ibangajnr27@gmail.com>
2022-12-14 18:28:34 +0100

    Updated pages views.py

--- pages/views.py
+++ pages/views.py
@@ -4,7 +4,6 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 import qrcode
 
-@login_required(login_url="login")
 def homePage(request):
     if request.user.is_authenticated:
         return redirect("dashboard")
