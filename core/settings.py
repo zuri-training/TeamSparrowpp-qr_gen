8eac1a2d6a1599e8a2b5f6e798bc9ff50c7c9773
nadisan <nadiawellela@gmail.com>
2022-12-16 14:04:12 +0300

    modified:   core/settings.py

--- core/settings.py
+++ core/settings.py
@@ -149,3 +149,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #set up for media folder
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+
+CSRF_TRUSTED_ORIGINS = ['https://teamsparrowpp-qrgen-production-6a43.up.railway.app/']
